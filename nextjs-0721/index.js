d25cfa110fec4d4d287b481d1970ff2419755eb3
taehun2123 <pyoneng_@naver.com>
2023-07-21 00:31:03 +0900

    next-js 백엔드 첫 연습 (연동함)

---
+++ nextjs-0721/index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const port = 5050;
+const data = require('../frontend/react-0718/src/data.js')
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send('book이나 bookInsert로 이동');
+})
+
+app.get('/book', (req, res) => {
+    res.send({ booklist: data });
+})
+
+app.post('/bookInsert', (req, res) => {
+  const requestname = req.body.name;
+  const requestcontent = req.body.content;
+  const newData = {
+    name : requestname,
+    content : requestcontent,
+  };
+  data.push(newData);
+  res.json(newData);
+})
+
+app.listen(port);
